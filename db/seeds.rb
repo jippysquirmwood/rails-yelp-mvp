2a8ff9cbd771c9da44a5b92ad2d294bdfac0684e
Jippy Squirmwood <jippysquirmwood@gmail.com>
2019-05-13 13:08:33 +0100

    models and schema completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  { name: "KFC", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "McDonalds", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "Dominos", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "Papa Johns", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "Nandos", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "Wasabi", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" },
+  { name: "Yo sushi", address: "1 Fleet Street, London, UK", category: "italian", phone_number: "+445830485343" }
+]
+
+Restaurant.create!(restaurants_attributes);
+puts 'Finished';
